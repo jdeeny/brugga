bd2b6f4cdb81503fd09aff5e1c23551b3f34513b
jdeeny <jdeeny@gmail.com>
2018-12-27 01:37:48 -0700

    flesh out credits a bit

--- gamestates/credits.lua
+++ gamestates/credits.lua
@@ -2,6 +2,27 @@ local class = require 'lib.middleclass'
 
 local Credits = class('Credits', Gamestate)
 
+function Credits:initialize()
+  self.super.initialize(self)
+
+  self.text =
+[[Game Name
+
+Dec 25 - Jan 31
+2018
+
+Person 1
+
+Person 2
+
+Person 3
+
+Person 4
+]]
+  print("Inside credits init")
+end
+
+
 function Credits:enter()
 end
 
@@ -12,6 +33,7 @@ function Credits:update(dt)
 end
 
 function Credits:draw()
+  -- Todo: draw text on the screen, scroll?
 end
 
 return Credits
