647452d578ec58f4d7d779ce49fc4188a7727388
jdeeny <jdeeny@gmail.com>
2018-12-28 00:53:59 -0700

    Change fonts, clean up titlepage some

--- gamestates/title.lua
+++ gamestates/title.lua
@@ -7,20 +7,27 @@ local Title = class('Title', Gamestate)
 function Title:initialize(name)
   Gamestate.initialize(self, name)
 
+  self.title = love.graphics.newText(gameWorld.assets.fonts.title(120), 'Game Title')
+
+  self.menuWidth = 900
+  self.menuHeight = 340
+
   self.menu = Menu:new({
     { kind='text', label='Credits', func=function() gameWorld.gameState:setState('credits') end },
     { kind='text', label='Play Game', func=function() gameWorld.gameState:setState('gameplay') end },
     { kind='slider', label='SFX', get=function() return gameWorld.sound.tags.sfx.volume end, set=function(value) gameWorld.sound.tags.sfx.volume = value end },
     { kind='slider', label='Music', get=function() return gameWorld.sound.tags.music.volume end, set=function(value) gameWorld.sound.tags.music.volume = value end },
     { kind='text', label='Exit to Desktop', func=function() love.event.push('quit') end },
-  })
+  }, self.menuWidth, self.menuHeight)
 
 end
 
 function Title:draw()
+  love.graphics.clear(1.0,1.0,1.0,1.0)
+  love.graphics.setColor(0.0,0.0,0.0,1.0)
+  love.graphics.draw(self.title, (1280 - self.title:getWidth()) / 2, 100)
   love.graphics.setColor(1.0,1.0,1.0,1.0)
-  love.graphics.rectangle('fill', 20, 10, 60, 20)
-  self.menu:drawAt(1280 / 2 - 150, 720 / 2 - 250, 300, 500)
+  self.menu:drawAt((1280 - self.menuWidth) / 2, 720 - self.menuHeight - 200 )
 end
 
 function Title:update()
--- ui/menu.lua
+++ ui/menu.lua
@@ -5,12 +5,15 @@ local Slider = require 'ui.slider'
 
 local Menu = class('Menu')
 
-function Menu:initialize(entries)
+function Menu:initialize(entries, w, h)
   local _entries = entries
+  self.w = w or 500
+  self.h = h or 300
   self.selected = 1
-  self.font = gameWorld.assets.fonts.generic(16)
-  self.vpad = 30
-  self.hpad = 15
+  self.font = gameWorld.assets.fonts.generic(42)
+  self.vsize = self.h / #_entries
+  self.vpad = 50
+  self.hpad = 25
   self.segments = 10
   self.sprites = {
     left = gameWorld.assets.sprites.ui.selectorLeft,
@@ -19,10 +22,8 @@ function Menu:initialize(entries)
     slideRight = gameWorld.assets.sprites.ui.sliderRight,
   }
 
-  self.width = 500
-
   for _, entry in ipairs(_entries) do
-    entry.text = Text:new(entry.label, { halign='center' }) --, font=self.font })
+    entry.text = Text:new(entry.label, { halign='center', font=self.font })
 
     if entry.kind == 'text' then
       entry.width = entry.text:getWidth()
@@ -60,21 +61,24 @@ function Menu:update(dt)
   end
 end
 
-function Menu:drawAt(x, y, w, h)
+function Menu:drawAt(x, y)
   love.graphics.setColor(colors.lightblue)
   for i, entry in ipairs(self.entries) do
-    local y = i * 30 + y
+    local y = i * self.vsize + y
     if entry.kind == 'text' then
-      entry.text:draw(x + self.sprites.left:getWidth() + self.hpad, y, w, h)
+      entry.text:draw(x, y, self.w, self.h)
       if i == self.selected then
-        love.graphics.draw(self.sprites.left, x, y)
-        love.graphics.draw(self.sprites.right, x + self.width - self.sprites.right:getWidth(), y)
+        local c = x + self.w / 2
+        local l = c - entry.text:getWidth() / 2 - self.hpad - self.sprites.left:getWidth()
+        local r = c + entry.text:getWidth() / 2 + self.hpad
+        love.graphics.draw(self.sprites.left, l, y + self.vsize / 2 - self.sprites.left:getHeight() / 2)
+        love.graphics.draw(self.sprites.right, r, y + self.vsize / 2 - self.sprites.right:getHeight() / 2)
       end
     elseif entry.kind == 'slider' then
       entry.slider:draw(x + self.sprites.left:getWidth() + self.hpad, y)
       if i == self.selected then
         love.graphics.draw(self.sprites.slideLeft, x, y)
-        love.graphics.draw(self.sprites.slideRight, x + self.width - self.sprites.right:getWidth(), y)
+        love.graphics.draw(self.sprites.slideRight, x + self.w - self.sprites.right:getWidth(), y)
       end
     end
 
