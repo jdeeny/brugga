d8ff347036b33bb4906cd9f077ee1af783541c54
jdeeny <jdeeny@gmail.com>
2018-12-28 02:27:15 -0700

    Pretty up the credits screen, needs to be switched to centered text, maybe in columns

--- gamestates/credits.lua
+++ gamestates/credits.lua
@@ -6,23 +6,30 @@ local Credits = class('Credits', Gamestate)
 function Credits:initialize(name)
   Gamestate.initialize(self, name)
 
-  self.text = love.graphics.newText(gameWorld.assets.fonts.generic(16),
-[[Game Name
+  self.textfont = gameWorld.assets.fonts.generic(30)
 
-Dec 25 - Jan 31
-2018
+  self.contributors = {
+    { name="John Deeny", contact="jdeeny@gmail.com", role="code", },
+    { name="Dieting Hippo", contact="@", role="code",},
+    { name="Drauc", contact="soundcloud.com/drauc", role="audio", },
+    { name="Raujinn", contact="@", role="art", },
+    { name="Ashe", contact="@", role="voice", },
+    -- { name="Xibanya", contact="@", role="voice", },
+    -- { name="Mihai", contact="@", role="voice", },
+  }
 
-Person 1
+  self.title = love.graphics.newText(gameWorld.assets.fonts.title(120), 'Game Title')
+  self.subtitle = love.graphics.newText(self.textfont, 'Dec 25 - Dec 31 2018')
 
-Person 2
+  -- Create text for each contributor
+  for i, contrib in ipairs(self.contributors) do
+    contrib.nameText = love.graphics.newText(self.textfont, contrib.name)
+    contrib.contactText = love.graphics.newText(self.textfont, contrib.contact)
+    contrib.roleText = love.graphics.newText(gameWorld.assets.fonts.generic(30), contrib.role)
+  end
 
-Person 3
 
-Person 4
-]]
-)
 
-  print("Inside credits init")
 end
 
 
@@ -37,7 +44,14 @@ function Credits:update(dt)
 end
 
 function Credits:draw()
-  love.graphics.draw(self.text) -- Todo: draw text on the screen, scroll?
+  love.graphics.clear(1.0, 1.0, 1.0, 1.0)
+  love.graphics.setColor(gameWorld.colors.lightblue)
+  love.graphics.draw(self.title, 1280 / 2 - self.title:getWidth() / 2, 50)
+  for i, contrib in ipairs(self.contributors) do
+    love.graphics.draw(contrib.nameText, 100, i * 100)
+    love.graphics.draw(contrib.roleText, 100, i * 100 + 30)
+    love.graphics.draw(contrib.contactText, 100, i * 100 + 60)
+  end
 end
 
 return Credits
