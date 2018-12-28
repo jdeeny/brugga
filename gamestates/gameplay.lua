caf9a99fa09c6e5d6453c5d8b051ea9442593600
braids <cymoro@gmail.com>
2018-12-28 02:05:22 -0800

    Brugga Implementation, Extra Debug Kabobbles
    dude.lua:
    - Spawn method added
    
    gameplay.lua:
    - Added brugga entity to gameplay scene.
    - patron is now patronA/patronB to test drink mix collisions.
    - Debug bar rectangles added.

--- gamestates/gameplay.lua
+++ gamestates/gameplay.lua
@@ -10,15 +10,21 @@ local Gameplay = class('Gameplay', Gamestate)
 -- Collision world
 local bumpWorld = bump.newWorld(50)
 -- Entities
-local patron = Enemy:new()
+local patronA = Enemy:new()
+local patronB = Enemy:new()
 local drink = Drink:new()
+local brugga = Dude:new()
 
 function Gameplay:initialize(name)
   Gamestate.initialize(self, name)
 
   print("Adding patron")
-  patron:addToWorld(bumpWorld)
-  patron:spawnEnemy("OnlyA", 1)
+  brugga:addToWorld(bumpWorld)
+  brugga:spawn()
+  patronA:addToWorld(bumpWorld)
+  patronA:spawnEnemy("OnlyA", 1)
+  patronB:addToWorld(bumpWorld)
+  patronB:spawnEnemy("OnlyB", 3)
   drink:addToWorld(bumpWorld)
   drink.row = 1
 
@@ -34,15 +40,26 @@ function  Gameplay:update(dt)
     gameWorld.gameState:setState('ending')
   end
 
-  -- Update entities
-  if patron.isActive then patron:update(dt) end
-  if drink.isActive then drink:update(dt) end
+  if gameWorld.playerInput:released('up') or gameWorld.playerInput:released('down') then
+    brugga:moveNone()
+  elseif gameWorld.playerInput:pressed('up') then
+    brugga:moveUp()
+  elseif gameWorld.playerInput:pressed('down') then
+    brugga:moveDown()
+  end
 
   -- Spawn drink if player presses throw
   if gameWorld.playerInput:pressed('throw') then
     drink.isActive = true
+    drink.row = brugga.row
     drink:sendLeft()
   end
+
+  -- Update entities
+  if brugga.isActive then brugga:update(dt) end
+  if drink.isActive then drink:update(dt) end
+  if patronA.isActive then patronA:update(dt) end
+  if patronB.isActive then patronB:update(dt) end
 
   self.nextReward = self.nextReward - dt
   if self.nextReward <= 0.0 then
@@ -52,9 +69,16 @@ function  Gameplay:update(dt)
 end
 
 function Gameplay:draw()
-  --love.graphics.setColor(1.0,0.0,0.0,1.0)
-  --love.graphics.rectangle('fill', 80,80,700,500)
-  patron:draw()
+  -- Debug bar boxes
+  love.graphics.setColor(.6, .2, .1, 100)
+  love.graphics.rectangle('fill', 300, 220, 680, 40)
+  love.graphics.rectangle('fill', 280, 320, 720, 40)
+  love.graphics.rectangle('fill', 260, 420, 760, 40)
+  love.graphics.rectangle('fill', 240, 520, 800, 40)
+
+  brugga:draw()
+  patronA:draw()
+  patronB:draw()
   drink:draw()
 end
 
