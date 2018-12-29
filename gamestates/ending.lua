3be9c1f33faeb504d9e609dcc324061305303dc5
jdeeny <jdeeny@gmail.com>
2018-12-28 23:54:52 -0700

    Start on improved ending, now with fonts and headings

--- gamestates/ending.lua
+++ gamestates/ending.lua
@@ -5,10 +5,24 @@ local Ending = class('Ending', Gamestate)
 
 function Ending:initialize(name)
   Gamestate.initialize(self, name)
+
+  self.font_banner = gameWorld.assets.fonts.generic(100)
+  self.font_heading = gameWorld.assets.fonts.generic(60)
+  self.font_text = gameWorld.assets.fonts.generic(40)
+  self.fade = 0.0
+
+  self.margin = 40
+  self.print_width = 1280 - 2 * self.margin
+
+  self.banner_y = 10
+  self.score_y = 120
+  self.patrons_y = 220
+  self.drinks_y = 540
 end
 
 function Ending:enter()
-  gameWorld.sound:playUi('gameOver')
+  self.fade = 1.0
+  flux.to(self, 2, { fade = 0.0 }):ease("quadinout")
   gameWorld.sound:playMusic('ending')
 end
 
@@ -20,7 +34,22 @@ function Ending:update(dt)
 end
 
 function Ending:draw()
-    love.graphics.draw(love.graphics.newText(gameWorld.assets.fonts.generic(16), "Score: " .. gameWorld.playerData.score), 1280 / 2, 720/2)
+  love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
+  love.graphics.setFont(self.font_banner)
+  love.graphics.printf("End Message", self.margin, self.banner_y, self.print_width, 'center')
+
+  love.graphics.setFont(self.font_heading)
+  love.graphics.printf("Score:", self.margin, self.score_y, self.print_width, 'left')
+  love.graphics.printf("Patrons:", self.margin, self.patrons_y, self.print_width, 'left')
+  love.graphics.printf("Drinks:", self.margin, self.drinks_y, self.print_width, 'left')
+
+
+
+
+  if self.fade > 0 then
+    love.graphics.setColor(1.0-self.fade, 1.0-self.fade, 1.0-self.fade, self.fade) --(0.0, 0.0, 0.0, self.fade)
+    love.graphics.rectangle('fill', 0, 0, 1280, 720)
+  end
 end
 
 return Ending
